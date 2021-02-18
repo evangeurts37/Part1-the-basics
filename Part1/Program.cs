e10d2145d9388b47c14f5457cf99f0c81d8238e7
Evan Geurts <GeurEvan189@GOTVDSB.CA>
2021-02-18 09:48:04 -0500

    Add project files.

---
+++ Part1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String FirstName = "Evan";
+            string FavMovie = "The Matrix";
+
+
+            Console.WriteLine("EEEEE        GGG     ");
+            Console.WriteLine("E           G   G  ");
+            Console.WriteLine("E           G ");
+            Console.WriteLine("EEE         G GGGG  ");
+            Console.WriteLine("E           G   G  ");
+            Console.WriteLine("E           G   G   ");
+            Console.WriteLine("EEEEE        GGG");
+
+
+
+            Console.WriteLine($"hello my name is {FirstName.ToLower()} and my favourite movie is {FavMovie.ToLower()}.");
+            FavMovie = FavMovie.ToUpper();
+            Console.WriteLine(FavMovie);
+            Console.WriteLine(FavMovie.Contains("THE"));
+            FavMovie = FavMovie.Replace("A", "@").Replace("E", "3");
+            Console.WriteLine(FavMovie);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
